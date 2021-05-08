983207c75e64414326030483c42dd11832426297
rafaeldellaquila <rafadellaquila@gmail.com>
2021-05-08 15:13:42 -0300

    :bug: fix type erros

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
-import { Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import { Text, Image, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 //Usando essa dependencia para contornar o SafeAreaView do Native que funciona apenas no IOS
 
-import wateringImg from '../assets/watering.png';
 import colors from '../styles/colors';
+import wateringImg from '../assets/watering.png';
+import { Button } from '../components/Button';
 
 export function Welcome() {
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.title}>
-        Gerencie{'\n'}suas plantas{'\n'}de formafácil
+        Gerencie{'\n'}suas plantas{'\n'}de forma fácil
       </Text>
       <Image source={wateringImg} style={styles.image} />
       <Text style={styles.subtitle}>
         Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
         sempre que precisar.
       </Text>
-      <TouchableOpacity style={styles.button} activeOpacity={0.8}>
-        <Text style={styles.buttonText}> &gt; </Text>
-      </TouchableOpacity>
+      <Button title=' &gt; ' />
     </SafeAreaView>
   );
 }
@@ -42,21 +41,6 @@ const styles = StyleSheet.create({
     fontSize: 18,
     paddingHorizontal: 20,
     color: colors.heading,
-  },
-  button: {
-    backgroundColor: colors.green,
-    textAlign: 'center',
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 16,
-    marginBottom: 40,
-    width: 56,
-    height: 56,
-  },
-
-  buttonText: {
-    color: colors.white,
-    fontSize: 24,
   },
 
   image: {
