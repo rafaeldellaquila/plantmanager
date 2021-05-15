124a30a2d7d5d48900c5fe3ecf48af474d033cb4
rafaeldellaquila <rafadellaquila@gmail.com>
2021-05-15 20:54:38 -0300

    :lipstick: starting styling the new page

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -7,6 +7,7 @@ import { Confirmation } from '../pages/Confirmation';
 import { PlantSelect } from '../pages/PlantSelect';
 
 import colors from '../styles/colors';
+import { PlantSave } from '../pages/PlantSave';
 
 const stackRoutes = createStackNavigator();
 
@@ -21,22 +22,14 @@ const AppRoutes: React.FC = () => (
       },
     }}
   >
-    <stackRoutes.Screen
-      name='Welcome'
-      component={Welcome}
-    />
+    <stackRoutes.Screen name='Welcome' component={Welcome} />
     <stackRoutes.Screen
       name='UserIndentification'
       component={UserIndentification}
     />
-    <stackRoutes.Screen
-      name='Confirmation'
-      component={Confirmation}
-    />
-    <stackRoutes.Screen
-      name='PlantSelect'
-      component={PlantSelect}
-    />
+    <stackRoutes.Screen name='Confirmation' component={Confirmation} />
+    <stackRoutes.Screen name='PlantSelect' component={PlantSelect} />
+    <stackRoutes.Screen name='PlantSave' component={PlantSave} />
   </stackRoutes.Navigator>
 );
 
