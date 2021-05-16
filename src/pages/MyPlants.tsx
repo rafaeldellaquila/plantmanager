a2b09823fac42dd49fdccfe312cd32301ebf0008
rafaeldellaquila <rafadellaquila@gmail.com>
2021-05-16 19:30:43 -0300

    :lipstick: create and init style MyPlants page

--- src/pages/MyPlants.tsx
+++ src/pages/MyPlants.tsx
@@ -1,2 +1,23 @@
 import React from 'react';
 import { StyleSheet, View, Text, Image } from 'react-native';
+import { Header } from '../components/Header';
+import colors from '../styles/colors';
+
+export function MyPlants() {
+  return (
+    <View style={styles.container}>
+      <Header />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 30,
+    PaddingTop: 50,
+    backgroundColor: colors.background,
+  },
+});
