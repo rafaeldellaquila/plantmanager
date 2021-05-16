f5f019f8452c6905c1399e33443cbaef22f6c854
rafaeldellaquila <rafadellaquila@gmail.com>
2021-05-16 19:44:01 -0300

    :recycle: refactoring and create a new interface

--- src/pages/Confirmation.tsx
+++ src/pages/Confirmation.tsx
@@ -1,39 +1,44 @@
 import React from 'react';
-import {
-  StyleSheet,
-  View,
-  Text,
-} from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { useNavigation } from '@react-navigation/core';
+import { useNavigation, useRoute } from '@react-navigation/core';
 
 import { Button } from '../components/Button';
 
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 
+interface Params {
+  title: string;
+  subtitle: string;
+  buttonTitle: string;
+  icon: 'smile' | 'hug';
+  nextScreen: string;
+}
+
+const emojis = {
+  hug: '🤗',
+  smile: '😃',
+};
+
 export function Confirmation() {
   const navigation = useNavigation();
+  const routes = useRoute();
+
+  const { title, subtitle, buttonTitle, icon, nextScreen } =
+    routes.params as Params;
 
   function handleMoveOn() {
-    navigation.navigate('PlantSelect');
+    navigation.navigate(nextScreen);
   }
   return (
     <SafeAreaView style={styles.content}>
       <View style={styles.content}>
-        <Text style={styles.emoji}>😃</Text>
-        <Text style={styles.title}>
-          Prontinho
-        </Text>
-        <Text style={styles.subtitle}>
-          Agora vamos começar a cuidar das suas
-          plantinhas com muito cuidado.
-        </Text>
+        <Text style={styles.emoji}>{emojis[icon]}</Text>
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.subtitle}>{subtitle}</Text>
         <View style={styles.footer}>
-          <Button
-            title='Começar'
-            onPress={handleMoveOn}
-          />
+          <Button title={buttonTitle} onPress={handleMoveOn} />
         </View>
       </View>
     </SafeAreaView>
